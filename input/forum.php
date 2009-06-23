fbd0a979d39c7b12681d9539053a32fb9f944488
wooptoo <wooptoo@gmail.com>
2009-06-23 19:39:18 +0300

    forum: post & topic add

--- input/forum.php
+++ input/forum.php
@@ -3,38 +3,96 @@
  * @file
  * Input forum.
  */
-	$topic = filter_input(INPUT_GET, 'topic', FILTER_VALIDATE_INT);
+	$topic['id'] =
+		filter_input(INPUT_GET, 'topic', FILTER_VALIDATE_INT); // topic number
+	$topic['add']['req'] =
+		$_POST['topic']['add']['req'];
 	$html_filter = new InputFilter($tags, $attr);
 
 /**
  * Input posts add.
  * @return null
  */
-function input_posts_add() {
-	$posts_add = $_POST['reply'];
-	if($_SESSION['login'] && $posts_add) {
-		global $topic;
-		global $html_filter;
+function input_post_add() {
+	global $topic, $html_filter;
 
-		$post = $_POST['post'];
-		$post = $html_filter->process($post);
+	$post['add']['body'] =
+		$html_filter->process($_POST['post']['add']['body']);
+	$post['add']['submit'] =
+		$_POST['post']['add']['submit'];
 
-		if($post) {
-			mysql_query(
-				'insert into posts
-				values (null, ' . esc($topic) . ', ' . $_SESSION['id_user'] . ', "' .
-				Date::to_sql('now') . '", "' . esc($post) . '")'
-			);
-			mysql_query(
-				'update topics
-				set date_modified="' . Date::to_sql('now') . '"
-				where id_topic=' . esc($topic)
-			);
-		}
+	if ($_SESSION['login']
+	&& $post['add']['body']
+	&& $post['add']['submit']) {
+		mysql_query (
+			'insert into posts
+			values (
+				null,'.
+				$topic['id'].','.
+				$_SESSION['id_user'].',
+				"'.Date::to_sql('now').'",
+				"'.esc($post['add']['body']).'"
+			)'
+		);
+		mysql_query(
+			'update topics
+			set date_modified="' . Date::to_sql('now') . '"
+			where id_topic=' . $topic['id']
+		);
 	}
 	return;
 }
-input_posts_add();
+input_post_add();
 
+
+/**
+ * Input topic add.
+ * @return null
+ */
+function input_topic_add() {
+	global $html_filter;
+
+	$topic['add']['title'] =
+		filter_var($_POST['topic']['add']['title'], FILTER_UNSAFE_RAW);
+	$topic['add']['body'] =
+		$html_filter->process($_POST['topic']['add']['body']);
+	$topic['add']['submit'] =
+		$_POST['topic']['add']['submit'];
+
+	if($topic['add']['title']
+	&& $topic['add']['body']
+	&& $topic['add']['submit']) {
+		// create topic
+		mysql_query (
+			'insert into topics
+			values (
+				null,'.
+				$_SESSION['id_user'].',
+				"'.Date::to_sql('now').'",
+				"'.esc($topic['add']['title']).'"
+			)'
+		);
+		// select created topic
+		$result = mysql_query (
+			'select id_topic
+			from topics
+			where title="'.$topic['add']['title'].'"'
+		);
+		$row = mysql_fetch_array($result);
+		// insert first post into topic
+		mysql_query (
+			'insert into posts
+			values (
+				null,'.
+				$row['id_topic'].','.
+				$_SESSION['id_user'].',
+				"'.Date::to_sql('now').'",
+				"'.esc($topic['add']['body']).'"
+			)'
+		);
+	}
+	return;
+}
+input_topic_add();
 
 ?>
--- pages/forum.php
+++ pages/forum.php
@@ -5,15 +5,35 @@
  */
 ?>
 <div id="forum">
+
 <div id="topics">
-<h2>Topics</h2>
+	<h2>Topics</h2>
+	<?php display_topic_list(); ?>
+</div>
+
+<div id="posts">
+<?php
+	if ($topic['add']['req']) {
+		display_topic_add();
+	} else
+	if ($topic['id']) {
+		display_post_list();
+		display_post_add();
+	}
+?>
+</div>
+
+</div>
+
+
 <?php
 /**
- * Display topics.
+ * Display list of topics
  * @return null
  */
-function display_topics() {
+function display_topic_list() {
 ?>
+<form action="<?php echo current_page(true); ?>" method="post">
 <table>
 	<thead>
 	<tr>
@@ -41,91 +61,107 @@ while ($row = mysql_fetch_array($result)) {
 		$row['id_topic'] . '">' . trim_title($row['title'], 20) . '</a>' ."\n".
 	'		</td>' . "\n" .
 	'		<td>' . $row['first_name'] . ' ' . $row['fam_name'] . '</td>' ."\n".
-	'		<td>' . Date::from_sql($row['date_modified']) . '</td>' . "\n" .
+	'		<td>' . Date::from_sql($row['date_modified']) . '</td>' . "\n".
 	'	</tr>' . "\n";
+}
+if ($_SESSION['login']) {
+?>
+	<tr>
+		<td></td>
+		<td></td>
+		<td>
+			<button name="topic[add][req]" value="true">Add topic</button>
+		</td>
+	</tr>
+<?php
 }
 ?>
 	</tbody>
 </table>
+</form>
 <?php
 	return;
 }
-display_topics();
-?>
-</div>
 
-<div id="posts">
-<?php
 /**
- * Display posts.
+ * Display list of posts
  * @return null
  */
-function display_posts() {
+function display_post_list() {
+	global $topic;
 
-		global $topic;
-		// View topic title.
-	if ($topic) {
-		$result = mysql_query('
-			select *
-			from topics
-			where id_topic=' . $topic . '
-		');
-		$row = mysql_fetch_array($result);
-		echo '<h2>' . $row['title'] . '</h2>' ."\n";
+	// View topic title.
+	$result = mysql_query('
+		select *
+		from topics
+		where id_topic=' . $topic['id'] . '
+	');
+	$row = mysql_fetch_array($result);
+	echo '<h2>' . $row['title'] . '</h2>' ."\n";
 
-
-		// View posts from topic.
-		$result = mysql_query('
-			select posts.id_post, posts.id_user, posts.date_created, posts.body,
-				users.first_name, users.fam_name
-			from posts
-			join users
-			on posts.id_user=users.id_user
-			where id_topic=' . $topic . '
-			order by date_created
-		');
-		while ($row = mysql_fetch_array($result)) {
-			echo
-			'<div id="post' . $row['id_post'] . '">' . "\n" .
-			'<p class="author">' . "\n" .
-			'	<a class="id" href="#post' . $row['id_post'] . '">#</a>' ."\n".
-			'	<span class="user">' .
-				$row['first_name'] . ' ' . $row['fam_name'] .
-				'</span>' . "\n" .
-			'	<span class="date">' . Date::from_sql($row['date_created']) .
+	// View posts from topic.
+	$result = mysql_query('
+		select posts.id_post, posts.id_user, posts.date_created, posts.body,
+			users.first_name, users.fam_name
+		from posts
+		join users
+		on posts.id_user=users.id_user
+		where id_topic=' . $topic['id'] . '
+		order by date_created
+	');
+	while ($row = mysql_fetch_array($result)) {
+		echo
+		'<div id="post' . $row['id_post'] . '">' . "\n" .
+		'<p class="author">' . "\n" .
+		'	<a class="id" href="#post' . $row['id_post'] . '">#</a>' ."\n".
+		'	<span class="user">' .
+			$row['first_name'] . ' ' . $row['fam_name'] .
 			'</span>' . "\n" .
-			'</p>' . "\n" .
-			'<div class="content">' . $row['body'] . '</div>' .
-			'</div>' .  "\n\n";
-		}
+		'	<span class="date">' . Date::from_sql($row['date_created']) .
+		'</span>' . "\n" .
+		'</p>' . "\n" .
+		'<div class="content">' . $row['body'] . '</div>' .
+		'</div>' .  "\n\n";
 	}
 	return;
 }
-display_posts();
-
 
 /**
- * Display posts > add.
+ * Display post add.
  * @return null
  */
-function display_posts_add() {
-		global $topic;
-
-		if ($_SESSION['login'] && $topic) {
+function display_post_add() {
+	global $topic;
+	if ($_SESSION['login']) {
 ?>
 	<form action="<?php echo current_page(true); ?>" method="post">
 		<div id="reply">
-			<h3><label for="textarea">Reply</label></h3>
-			<textarea name="post" rows="5" cols="57" id="textarea"></textarea><br>
-			<input name="reply" type="submit" value="Post reply">
+			<h3>
+				<label for="textarea">Reply</label>
+			</h3>
+			<textarea name="post[add][body]" rows="5" cols="57"
+				id="textarea"></textarea><br>
+			<button name="post[add][submit]" value="true">Post reply</button>
 		</div>
 	</form>
 <?php
-		}
+	}
 	return;
 }
-display_posts_add();
-?>
-</div>
 
-</div>
+/**
+ * Display topic add.
+ * @return null
+ */
+function display_topic_add() {
+?>
+	<form action="<?php echo current_page(true); ?>" method="post">
+		<input name="topic[add][title]" type="text" class="page_title"><br><br>
+		<textarea name="topic[add][body]" rows="10" cols="57"
+			id="textarea"></textarea><br>
+		<button name="topic[add][submit]" value="true">Submit</button>
+	</form>
+<?php
+	return;
+}
+?>
