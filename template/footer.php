017d539ba4c9fcffa9c9bb5ed7790859d11c2990
wooptoo <wooptoo@gmail.com>
2009-10-21 01:39:52 +0300

    do not show no. of queries by default

--- template/footer.php
+++ template/footer.php
@@ -2,7 +2,7 @@
 <p id="footer">All content &copy; <?php echo $site['name']; ?> |
  Powered by <a href="<?php echo $app['site']; ?>">Studentware</a>
  <?php echo $app['ver']; ?>
- <span id="queries"> | <?php echo $queryCount; ?> queries</span>
+ <!-- <span id="queries"> | <?php echo $queryCount; ?> queries</span> -->
 </p>
 
 <script type="text/javascript" src="scripts/loader.js"></script>
