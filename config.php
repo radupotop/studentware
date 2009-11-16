cd27b58fe5a16aa211abd25f1a10ecec70bcea08
wooptoo <wooptoo@gmail.com>
2009-11-16 02:54:07 +0200

    ver bump

--- config.php
+++ config.php
@@ -113,7 +113,7 @@ $db = array (
  */
 $app = array (
 	'site' => 'http://bitbucket.org/wooptoo/studentware/',
-	'ver' => '0.3.9',
+	'ver' => '0.3.10',
 	'log' => array (
 		'enabled' => false,
 		// absolute path to log file
