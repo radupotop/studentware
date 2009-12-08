a58134f32c220a44b17c4c55f301f8a82062eaf0
wooptoo <wooptoo@gmail.com>
2009-12-08 14:56:06 +0200

    fixed install, better Model->view()
    --HG--
    branch : dev

--- functions/class.model.php
+++ functions/class.model.php
@@ -9,9 +9,15 @@ class Model {
 	 * @param string $table
 	 * @return array $data
 	 */
-	function view($table) {
+	function view($table, $field=null, $value=null, $limit=null) {
 		$pk = 'id_'.substr($table, 0, -1);
-		$query = sprintf('select * from %s', $table);
+
+		$query = sprintf('select * from %s ', $table);
+		if($field && $value)
+			$query .= sprintf('where %s like "%s" ', $field, esc($value));
+		if($limit)
+			$query .= sprintf('limit %s ', $limit);
+
 		$result = mysql_query($query);
 		queryCount();
 		while($row = mysql_fetch_assoc($result))
