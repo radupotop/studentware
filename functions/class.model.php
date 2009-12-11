8af1f1b7079384086d6c6838be0c85c58bea4532
wooptoo <wooptoo@gmail.com>
2009-12-11 03:11:40 +0200

    Model delete, viewLastID, pk
    --HG--
    branch : dev

--- functions/class.model.php
+++ functions/class.model.php
@@ -3,6 +3,18 @@
  * @class Model
  */
 class Model {
+
+	/**
+	 * Get a table's PK name.
+	 *
+	 * @param string $table
+	 * @return string $pk
+	 */
+	function pk($table) {
+		$pk = 'id_'.substr($table, 0, -1);
+		return $pk;
+	}
+
 	/**
 	 * Get data from table.
 	 *
@@ -10,7 +22,7 @@ class Model {
 	 * @return array $data
 	 */
 	function view($table, $field=null, $value=null, $limit=null) {
-		$pk = 'id_'.substr($table, 0, -1);
+		$pk = $this->pk($table);
 
 		$query = sprintf('select * from %s ', $table);
 		if($field && $value)
@@ -26,6 +38,22 @@ class Model {
 	}
 
 	/**
+	 * View the ID of the last inserted row from table.
+	 *
+	 * @param string $table
+	 * @return int $id
+	 */
+	function viewLastID($table) {
+		$pk = $this->pk($table);
+		$query = sprintf('select * from %s order by %s desc limit 1', $table, $pk);
+		$result = mysql_query($query);
+		queryCount();
+		$row = mysql_fetch_assoc($result);
+		$id = (int) $row[$pk];
+		return $id;
+	}
+
+	/**
 	 * Add data to table.
 	 */
 	function add($table, $data=array()) {
@@ -38,11 +66,22 @@ class Model {
 		$query = sprintf('insert into %s (%s) values (%s)', $table, $keyList, $valueList);
 		$result = mysql_query($query);
 		queryCount();
-		if($result)
-			$ret = true;
-		else
-			$ret = false;
-		return $ret;
+		return $result;
+	}
+
+	/**
+	 * Delete data from table.
+	 *
+	 * @param string $table
+	 * @param int $id
+	 * @return bool $result
+	 */
+	function delete($table, $id=int) {
+		$pk = $this->pk($table);
+		$query = sprintf('delete from %s where %s = %s', $table, $pk, $id);
+		$result = mysql_query($query);
+		queryCount();
+		return $result;
 	}
 }
 ?>
